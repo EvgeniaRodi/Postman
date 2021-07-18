e7d6bce9942da25bd0d3e3b8eeb81e4517e99231
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-07-18 23:08:14 +0300

    SuccessTest

--- src/test/java/ru/netology/postman/PostmanTest.java
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 class PostmanTest {
     @Test
-    void shouldPostData(){
+    void shouldPostData (){
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
@@ -17,7 +17,21 @@ class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
+        ;
+
+    }
+    @Test
+    void shouldPostDataContentType(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
         ;
 
     }
